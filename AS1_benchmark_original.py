596586f7a6b9c05cb3e5bdf16bcae338fbc82253
FrankShi9 <53264047+FrankShi9@users.noreply.github.com>
2021-11-21 21:05:55 +0000

    informal full version of cw and cascade before formal modification with foolbox external attack and robust test with not full version of GT attack with a version of Deepfool full version official ATLD adv train not dynamically tested

--- AS1_benchmark_original.py
+++ AS1_benchmark_original.py
@@ -9,7 +9,7 @@
 ###
 ### The score is based on both algorithms. 
 ############################################################################
-import copy
+
 
 import numpy as np
 import pandas as pd
@@ -42,11 +42,12 @@ parser.add_argument('--no-cuda', action='store_true', default=False,
 parser.add_argument('--seed', type=int, default=1, metavar='S',
                     help='random seed (default: 1)')
 
-args = parser.parse_args(args=[])
+
+args = parser.parse_args(args=[]) 
 
 # judge cuda is available or not
 use_cuda = not args.no_cuda and torch.cuda.is_available()
-# device = torch.device("cuda" if use_cuda else "cpu")
+#device = torch.device("cuda" if use_cuda else "cpu")
 device = torch.device("cpu")
 
 torch.manual_seed(args.seed)
@@ -55,20 +56,17 @@ kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
 ############################################################################
 ################    don't change the below code    #####################
 ############################################################################
-train_set = torchvision.datasets.FashionMNIST(root='../data', train=True, download=True,
-                                              transform=transforms.Compose([transforms.ToTensor()]))
+train_set = torchvision.datasets.FashionMNIST(root='../data', train=True, download=True, transform=transforms.Compose([transforms.ToTensor()]))
 train_loader = DataLoader(train_set, batch_size=args.batch_size, shuffle=True)
 
-test_set = torchvision.datasets.FashionMNIST(root='../data', train=False, download=True,
-                                             transform=transforms.Compose([transforms.ToTensor()]))
+test_set = torchvision.datasets.FashionMNIST(root='../data', train=False, download=True, transform=transforms.Compose([transforms.ToTensor()]))
 test_loader = DataLoader(test_set, batch_size=args.batch_size, shuffle=True)
-
 
 # define fully connected network
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
-        self.fc1 = nn.Linear(28 * 28, 128)
+        self.fc1 = nn.Linear(28*28, 128)
         self.fc2 = nn.Linear(128, 64)
         self.fc3 = nn.Linear(64, 32)
         self.fc4 = nn.Linear(32, 10)
@@ -84,141 +82,50 @@ class Net(nn.Module):
         output = F.log_softmax(x, dim=1)
         return output
 
-
 ##############################################################################
 #############    end of "don't change the below code"   ######################
 ##############################################################################
 
-def deepfool(image, net, num_classes=10, overshoot=0.02, max_iter=50):
-    """
-       :param image: Image of size HxWx3
-       :param net: network (input: images, output: values of activation **BEFORE** softmax).
-       :param num_classes: num_classes (limits the number of classes to test against, by default = 10)
-       :param overshoot: used as a termination criterion to prevent vanishing updates (default = 0.02).
-       :param max_iter: maximum number of iterations for deepfool (default = 50)
-       :return: minimal perturbation that fools the classifier, number of iterations that it required, new estimated_label and perturbed image
-    """
-    is_cuda = torch.cuda.is_available()
-
-    if is_cuda:
-        print("Using GPU")
-        image = image.cuda()
-        net = net.cuda()
-    else:
-        print("Using CPU")
-
-    f_image = net.forward(Variable(image[None, :, :, :], requires_grad=True)).data.cpu().numpy().flatten()
-    I = (np.array(f_image)).flatten().argsort()[::-1]
-
-    I = I[0:num_classes]
-    label = I[0]
-
-    input_shape = image.cpu().numpy().shape
-    pert_image = copy.deepcopy(image)
-    w = np.zeros(input_shape)
-    r_tot = np.zeros(input_shape)
-
-    loop_i = 0
-
-    x = Variable(pert_image[None, :], requires_grad=True)
-    fs = net.forward(x)
-    fs_list = [fs[0, I[k]] for k in range(num_classes)]
-    k_i = label
-
-    while k_i == label and loop_i < max_iter:
-
-        pert = np.inf
-        fs[0, I[0]].backward(retain_graph=True)
-        grad_orig = x.grad.data.cpu().numpy().copy()
-
-        for k in range(1, num_classes):
-            torch.autograd.gradcheck.zero_grad(x)
-            # zero_gradients(x)
-
-            fs[0, I[k]].backward(retain_graph=True)
-            cur_grad = x.grad.data.cpu().numpy().copy()
-
-            # set new w_k and new f_k
-            w_k = cur_grad - grad_orig
-            f_k = (fs[0, I[k]] - fs[0, I[0]]).data.cpu().numpy()
-
-            pert_k = abs(f_k) / np.linalg.norm(w_k.flatten())
-
-            # determine which w_k to use
-            if pert_k < pert:
-                pert = pert_k
-                w = w_k
-
-        # compute r_i and r_tot
-        # Added 1e-4 for numerical stability
-        r_i = (pert + 1e-4) * w / np.linalg.norm(w)
-        r_tot = np.float32(r_tot + r_i)
-
-        if is_cuda:
-            pert_image = image + (1 + overshoot) * torch.from_numpy(r_tot).cuda()
-        else:
-            pert_image = image + (1 + overshoot) * torch.from_numpy(r_tot)
-
-        x = Variable(pert_image, requires_grad=True)
-        fs = net.forward(x)
-        k_i = np.argmax(fs.data.cpu().numpy().flatten())
-
-        loop_i += 1
-
-    r_tot = (1 + overshoot) * r_tot
-    return pert_image
-    # return r_tot, loop_i, label, k_i, pert_image
-
-
 'generate adversarial data, you can define your adversarial method'
-
-
 def adv_attack(model, X, y, device):
-    for X, y in enumerate(train_loader):
-        X = deepfool(X, model, num_classes=10, overshoot=0.02, max_iter=50)
-    return X
-    # X_adv = Variable(X.data)
-
+    X_adv = Variable(X.data)
+    
     ################################################################################################
     ## Note: below is the place you need to edit to implement your own attack algorithm
     ################################################################################################
-
-    # random_noise = torch.FloatTensor(*X_adv.shape).uniform_(-0.1, 0.1).to(device)
-    # X_adv = Variable(X_adv.data + random_noise)
-
+    
+    random_noise = torch.FloatTensor(*X_adv.shape).uniform_(-0.1, 0.1).to(device)
+    X_adv = Variable(X_adv.data + random_noise)
+    
     ################################################################################################
     ## end of attack method
     ################################################################################################
-    # return X_adv
-
+    return X_adv
 
 'train function, you can use adversarial training'
-
-
 def train(args, model, device, train_loader, optimizer, epoch):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        data = data.view(data.size(0), 28 * 28)
-
-        # use adverserial data to train the defense model
-        # adv_data = adv_attack(model, data, target, device=device)
-
-        # clear gradients
+        data = data.view(data.size(0), 28*28)
+        
+        #use adverserial data to train the defense model
+        #adv_data = adv_attack(model, data, target, device=device)
+        
+        #clear gradients
         optimizer.zero_grad()
-
-        # compute loss
-        # loss = F.cross_entropy(model(adv_data), target)
+        
+        #compute loss
+        #loss = F.cross_entropy(model(adv_data), target)
         loss = F.cross_entropy(model(data), target)
-
-        # get gradients and update
+        
+        #get gradients and update
         loss.backward()
         optimizer.step()
+        
 
 
 'predict function'
-
-
 def eval_test(model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -226,7 +133,7 @@ def eval_test(model, device, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            data = data.view(data.size(0), 28 * 28)
+            data = data.view(data.size(0),28*28)
             output = model(data)
             test_loss += F.cross_entropy(output, target, size_average=False).item()
             pred = output.max(1, keepdim=True)[1]
@@ -235,7 +142,6 @@ def eval_test(model, device, test_loader):
     test_accuracy = correct / len(test_loader.dataset)
     return test_loss, test_accuracy
 
-
 def eval_adv_test(model, device, test_loader):
     model.eval()
     test_loss = 0
@@ -243,7 +149,7 @@ def eval_adv_test(model, device, test_loader):
     with torch.no_grad():
         for data, target in test_loader:
             data, target = data.to(device), target.to(device)
-            data = data.view(data.size(0), 28 * 28)
+            data = data.view(data.size(0),28*28)
             adv_data = adv_attack(model, data, target, device=device)
             output = model(adv_data)
             test_loss += F.cross_entropy(output, target, size_average=False).item()
@@ -253,59 +159,53 @@ def eval_adv_test(model, device, test_loader):
     test_accuracy = correct / len(test_loader.dataset)
     return test_loss, test_accuracy
 
-
 'main function, train the dataset and print train loss, test loss for each epoch'
-
-
 def train_model():
     model = Net().to(device)
 
-    ################################################################################################
+ ################################################################################################
     ## Note: below is the place you need to edit to implement your own training algorithm
     ##       You can also edit the functions such as train(...). 
     ################################################################################################
     optimizer = optim.SGD(model.parameters(), lr=args.lr)
     for epoch in range(1, args.epochs + 1):
         start_time = time.time()
-
-        # training
+        
+        #training
         train(args, model, device, train_loader, optimizer, epoch)
-
-        # get trnloss and testloss
+        
+        #get trnloss and testloss
         trnloss, trnacc = eval_test(model, device, train_loader)
         advloss, advacc = eval_adv_test(model, device, train_loader)
-
-        # print trnloss and testloss
-        print('Epoch ' + str(epoch) + ': ' + str(int(time.time() - start_time)) + 's', end=', ')
+        
+        #print trnloss and testloss
+        print('Epoch '+str(epoch)+': '+str(int(time.time()-start_time))+'s', end=', ')
         print('trn_loss: {:.4f}, trn_acc: {:.2f}%'.format(trnloss, 100. * trnacc), end=', ')
         print('adv_loss: {:.4f}, adv_acc: {:.2f}%'.format(advloss, 100. * advacc))
-
+    
     ################################################################################################
     ## end of training method
     ################################################################################################
-
-    # save the model
-    # torch.save(model.state_dict(), str(id_) + '.pt')
+    
+    #save the model
+    torch.save(model.state_dict(), str(id_)+'.pt')
     return model
 
-
 'compute perturbation distance'
-
-
 def p_distance(model, train_loader, device):
     p = []
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
-        data = data.view(data.size(0), 28 * 28)
+        data = data.view(data.size(0),28*28)
         adv_data = adv_attack(model, data, target, device=device)
-        p.append(torch.norm(data - adv_data, float('inf')))
-    print('epsilon p: ', max(p))
+        p.append(torch.norm(data-adv_data, float('inf')))
+    print('epsilon p: ',max(p))
 
-
+    
 ################################################################################################
 ## Note: below is for testing/debugging purpose, please comment them out in the submission file
 ################################################################################################
-
+    
 'Comment out the following command when you do not want to re-train the model'
 'In that case, it will load a pre-trained model you saved in train_model()'
 model = train_model()
