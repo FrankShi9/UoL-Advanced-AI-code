698da7cc7be0c6a0737950cca26905671addcd7f
FrankShi9 <53264047+FrankShi9@users.noreply.github.com>
2021-11-25 23:00:58 +0000

    informal full version of cw and cascade before formal modification with foolbox external attack and robust test with not full version of GT attack with a version of Deepfool full version official ATLD adv train not dynamically tested 25/11/2021

--- competition_201600830.py
+++ competition_201600830.py
@@ -179,6 +179,63 @@ def pgd_whitebox(model, X, y, epsilon=args.epsilon, num_steps=args.num_steps, st
     return X_pgd
 
 
+# CW-L2 Attack
+# Based on the paper, i.e. not exact same version of the code on https://github.com/carlini/nn_robust_attacks
+# (1) Binary search method for c, (2) Optimization on tanh space, (3) Choosing method best l2 adversaries is NOT IN THIS CODE.
+def cw_l2_attack(model, X, y, targeted=False, c=1e-4, kappa=0, max_iter=1000, learning_rate=0.01):
+    images = X.to(device)
+    labels = y.to(device)
+
+    # Define f-function
+    def f(x):
+
+        outputs = model(x)
+        one_hot_labels = torch.eye(len(outputs[0]))[labels].to(device)
+
+        i, _ = torch.max((1 - one_hot_labels) * outputs, dim=1)
+        j = torch.masked_select(outputs, one_hot_labels.byte())
+
+        # If targeted, optimize for making the other class most likely
+        if targeted:
+            return torch.clamp(i - j, min=-kappa)
+
+        # If untargeted, optimize for making the other class most likely
+        else:
+            return torch.clamp(j - i, min=-kappa)
+
+    w = torch.zeros_like(images, requires_grad=True).to(device)
+
+    optimizer = optim.Adam([w], lr=learning_rate)
+
+    prev = 1e10
+
+    for step in range(max_iter):
+
+        a = 1 / 2 * (nn.Tanh()(w) + 1)
+
+        loss1 = nn.MSELoss(reduction='sum')(a, images)
+        loss2 = torch.sum(c * f(a))
+
+        cost = loss1 + loss2
+
+        optimizer.zero_grad()
+        cost.backward()
+        optimizer.step()
+
+        # Early Stop when loss does not converge.
+        if step % (max_iter // 10) == 0:
+            if cost > prev:
+                print('Attack Stopped due to CONVERGENCE....')
+                return a
+            prev = cost
+
+        print('- Learning Progress : %2.2f %%        ' % ((step + 1) / max_iter * 100), end='\r')
+
+    attack_images = 1 / 2 * (nn.Tanh()(w) + 1)
+
+    return attack_images
+
+
 # C&W attack: sub-optimal for single un-composite attack
 def calc_cw(model, X, epsilon=args.epsilon):
     noise = torch.FloatTensor(*X.shape).uniform_(-0.1, 0.1).to(device)
